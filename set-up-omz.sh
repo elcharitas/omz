cfbbd98a2860ecac393ff711f60b3fcf0dcf4ad6
Jonathan Irhodia <jonathanirhodia@gmail.com>
2023-01-26 22:54:17 +0100

    chore: add ZSH_CUSTOM declaration

--- set-up-omz.sh
+++ set-up-omz.sh
@@ -1,4 +1,5 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+export ZSH_CUSTOM=~/.oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
