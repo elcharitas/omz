e178af62ed146e2149f82aab0b77a51760626279
Jonathan Irhodia <jonathanirhodia@gmail.com>
2023-02-01 17:51:04 +0100

    fix: zshrc not loaded

--- set-up-omz.sh
+++ set-up-omz.sh
@@ -1,5 +1,7 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
 mv .zshrc ~/.zshrc
+cd ~
+source .zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
